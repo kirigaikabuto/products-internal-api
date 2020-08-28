a8936173a29a7e8f33fc4e80b47871c0fc1664e9
Tleugazy Yerassyl <40234888+kirigaikabuto@users.noreply.github.com>
2020-08-28 14:25:52 +0600

    first load

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/kirigaikabuto/products-internal-api
+
+go 1.13
+
+require (
+	github.com/djumanoff/amqp v1.0.5
+	github.com/kirigaikabuto/products-lib v1.0.1-0.20200827162212-67b616112a90
+	github.com/sirupsen/logrus v1.6.0 // indirect
+	github.com/streadway/amqp v1.0.0 // indirect
+	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a // indirect
+	golang.org/x/sys v0.0.0-20200826173525-f9321e4c35a6 // indirect
+)
---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"github.com/djumanoff/amqp"
+	"log"
+	products "github.com/kirigaikabuto/products-lib"
+)
+
+var cfg = amqp.Config{
+	Host: "localhost",
+	VirtualHost: "",
+	User: "",
+	Password: "",
+	Port: 5672,
+	LogLevel: 5,
+}
+
+var srvCfg = amqp.ServerConfig{
+	ResponseX: "response",
+	RequestX: "request",
+}
+
+func main(){
+	fmt.Println("Start")
+
+	sess := amqp.NewSession(cfg)
+
+	if err := sess.Connect(); err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer sess.Close()
+
+	srv, err := sess.Server(srvCfg)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	postgreConf := products.Config{
+		Host:             "localhost",
+		User:             "kirito",
+		Password:         "passanya",
+		Port:             5432,
+		Database:         "crm",
+		ConnectionString: "",
+		Params:           "sslmode=disable",
+	}
+	productStore, err := products.NewPostgreStore(postgreConf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	productService := products.NewProductService(productStore)
+	productsAmqpEndpoints := products.NewAMQPEndpointFactory(productService)
+	srv.Endpoint("products_lib.get",productsAmqpEndpoints.GetProductByIdAMQPEndpoint())
+    srv.Endpoint("products_lib.create", productsAmqpEndpoints.CreateProductAMQPEndpoint())
+	srv.Endpoint("products_lib.list", productsAmqpEndpoints.ListProductsAMQPEndpoint())
+	srv.Endpoint("products_lib.delete", productsAmqpEndpoints.DeleteProductAMQPEndpoint())
+
+	if err := srv.Start(); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("End")
+}
